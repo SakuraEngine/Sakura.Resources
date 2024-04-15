e2df8b8b03a7a1c158f59c5c1f69045e316c3f96
zhu-rong <211103376@qq.com>
2024-04-16 00:24:01 +0800

    update ci for output chagne info

--- release_tools/build_manifest.lua
+++ release_tools/build_manifest.lua
@@ -1,16 +1,32 @@
 import("core.base.json")
 function main(package_path, manifest_path)
-    printf("package_path: %s, manifest_path: %s\n", package_path, manifest_path)
+    printf("===== [package_path]: %s\n", package_path)
+    printf("===== [manifest_path]: %s\n", manifest_path)
 
+    -- get packages
     local packages = os.files(path.join(os.curdir(), package_path))
     local manifest = {}
+
+    -- load old manifest if exists
+    local old_manifest = {}
+    if os.isfile(manifest_path) then
+        old_manifest = json.loadfile(manifest_path)
+    end
+
     for _, package in ipairs(packages) do
         package_name = path.filename(package)
         
         if package_name ~= path.filename(manifest_path) then
             package_sha = hash.sha256(package)
-            printf("package: %s, sha256: %s\n", package_name, package_sha)
             manifest[package_name] = package_sha
+
+            old_package_sha = old_manifest[package_name]
+
+            if old_package_sha and old_package_sha ~= package_sha then
+                printf("[KEEP] package: %s, sha256: %s\n", package_name, package_sha)
+            else
+                printf("[CHANGED] package: %s, sha256: %s, old_sha256: %s\n", package_name, package_sha, old_package_sha)
+            end
         end
     end
     json.savefile(path.join(os.curdir(), manifest_path), manifest)
