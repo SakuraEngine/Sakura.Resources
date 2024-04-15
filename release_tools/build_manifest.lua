c60d6e697739f6c654a42231b6a67088774ed850
zhu-rong <211103376@qq.com>
2024-04-16 00:31:26 +0800

    fix ci

--- release_tools/build_manifest.lua
+++ release_tools/build_manifest.lua
@@ -23,9 +23,9 @@ function main(package_path, manifest_path)
             old_package_sha = old_manifest[package_name]
 
             if old_package_sha and old_package_sha ~= package_sha then
-                printf("[KEEP] package: %s, sha256: %s\n", package_name, package_sha)
+                printf("[CHANGED] package: %s\n\tsha256: %s\n\told_sha256: %s\n", package_name, package_sha, old_package_sha)
             else
-                printf("[CHANGED] package: %s, sha256: %s, old_sha256: %s\n", package_name, package_sha, old_package_sha)
+                printf("[KEEP] package: %s\n\tsha256: %s\n", package_name, package_sha)
             end
         end
     end
